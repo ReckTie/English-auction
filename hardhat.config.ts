a31b74e02565d28c94b2775a794efc8ed8fc6136
Vadim Sh0 <happydaysknow@gmail.com>
2022-08-02 14:44:20 +0300

    package init

---
+++ hardhat.config.ts
@@ -0,0 +1,95 @@
+import * as dotenv from 'dotenv';
+import "hardhat-gas-reporter";
+import "dotenv";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "solidity-coverage";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "./tasks";
+import 'hardhat-deploy';
+import "@openzeppelin/hardhat-upgrades";
+
+dotenv.config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import("hardhat/config").HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      gasPrice: 150000000000,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      tags: []
+    },
+    hardhat: {
+      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+      mining: {
+        auto: true,
+        // interval: 5000
+      },
+      gasPrice: 0
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/xwGYkQ0PNUKcm4JG4Zg1dTx2sbEF81vh",
+      chainId: 80001,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygon: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/mmAWeNGJjtvISsTT4drie6C5Z500GrLD",
+      chainId: 137,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    bsc: {
+      url: "https://bscrpc.com",
+      chainId: 56,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    firstTestWallet: {
+      default: 1,
+    },
+    secondTestWallet: {
+      default: 2,
+    },
+    thirdTestWallet: {
+      default: 3,
+    },
+    fourthTestWallet: {
+      default: 4,
+    }
+  }
+};
